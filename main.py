f16b0cb436bbfd68b57fa6c52f851a3d84688af6
r888800009 <r888800009@users.noreply.github.com>
2018-10-20 02:39:39 +0800

    Add line.py modules

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
+from importlib import import_module
 import pprint
 from init import *
 
+print("Hello there, it's r809's bot")
+print("/*------------------------*/")
 # check config is exists
-config = ""
+configFile = ""
 try:
-    config = open("config.json").read()
+    configFile = open("config.json").read()
 except FileNotFoundError:
     createNewConfige()
-    config = open("config.json").read()
+    configFile = open("config.json").read()
 
-data = json.loads(config)
-print(json.dumps(data, indent =4))
+config = json.loads(configFile)
+print("config dump")
+print(json.dumps(config, indent = 4))
+
+
+print("/*------------------------*/")
+print("Loading apis")
+print(config["Output"])
+
+connectType = {
+        "LineAPI": lambda : import_module("modules.line")
+        }
+for apis in config["Output"]:
+    print(apis)
+    connectType.get(apis)()
