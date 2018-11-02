d24ac3320cdee7b5b231020d2592845d4ea0312f
r888800009 <r888800009@users.noreply.github.com>
2018-11-02 16:33:52 +0800

    Rename chat api .py name

--- modules/line_api.py
+++ modules/line_api.py
@@ -1,6 +1,6 @@
+"""modules for Line chat API"""
 from flask import Flask, request, abort
 import jieba
-import config as cf
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -14,13 +14,15 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
+import config as cf
+
 print("Start line api")
 app = Flask(__name__)
 
-lineBotaAPI = LineBotApi(cf.config["LineAPI"]["AccessToken"])
-handler     = WebhookHandler(cf.config["LineAPI"]["Secret"])
+lineBotaAPI = LineBotApi(cf.CONFIG["LineAPI"]["AccessToken"])
+handler     = WebhookHandler(cf.CONFIG["LineAPI"]["Secret"])
 
-@app.route(cf.config["LineAPI"]["Path"], methods = ['POST'])
+@app.route(cf.CONFIG["LineAPI"]["Path"], methods = ['POST'])
 def linebot():
     sign = request.headers['X-Line-Signature']
 
@@ -34,11 +36,11 @@ def linebot():
 
     return 'OK'
 
-@handler.add(MessageEvent, message = TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     seg_list = jieba.cut(event.message.text)
     lineBotaAPI.reply_message(
         event.reply_token,
-        TextSendMessage(text = ", ".join(seg_list)))
+        TextSendMessage(text=", ".join(seg_list)))
 
 app.run()
--- modules/telegram_api.py
+++ modules/telegram_api.py
@@ -1,16 +1,19 @@
-from telegram.ext import Updater, CommandHandler
+"""modules for telegram API"""
 import jieba
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
 import config as cf
 print("Start telegram API")
 
-updater = Updater(cf.config["TelegramAPI"]["Token"])
+updater = Updater(cf.CONFIG["TelegramAPI"]["Token"])
 dispatcher = updater.dispatcher
 
 def echo(bot, update):
+    print(update.message)
     seg_list = jieba.cut(update.message.text)
     bot.send_message(chat_id=update.message.chat_id, text=", ".join(seg_list))
 
-from telegram.ext import MessageHandler, Filters
+
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
